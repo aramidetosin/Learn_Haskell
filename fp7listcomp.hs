9babf90f2cccd5cc2a890bdfb4366aaddeef9657
Aramide Oluwatosin <aoluwatosin10@gmail.com>
2022-01-04 02:00:22 +0000

    Learning Haskell

--- fp7listcomp.hs
+++ fp7listcomp.hs
@@ -7,4 +7,7 @@ cons = concat [[1,2,3], [4,5,6],[1]]
 xeven = [x | x <- [1..10], even x]
 factors :: Int -> [Int]
 factors n = [x | x <- [1..n], mod n x == 0]
+prime :: Int -> Bool
+prime n = factors n == [1,n]
+primes n = [x | x <- [2..n], prime x]
 
