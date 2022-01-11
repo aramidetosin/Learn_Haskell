27f0f6d275fc8e1d69f691d665e3894c1fa3a133
Aramide Oluwatosin <aoluwatosin10@gmail.com>
2022-01-11 01:43:54 +0000

    Learning Haskell

--- fp8recursivefunction.hs
+++ fp8recursivefunction.hs
@@ -1,3 +1,5 @@
+import Prelude hiding ((++))
+-- import Prelude hiding (zip)
 factorial :: Int -> Int
 factorial n = product [1..n]
 
@@ -21,3 +23,17 @@ fibo :: (Eq a, Num a, Num p) => a -> p
 fibo 0 = 0
 fibo 1 = 1
 fibo n = fibo(n-2)+fibo(n-1)
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+drops :: Int -> [a] -> [a]
+drops _ [] = []
+drops 0 xs = xs
+drops n (_:xs) = drops (n-1) xs
+
+(++) :: [a] -> [a] -> [a]
+[] ++ ys = ys
+(x:xs) ++ ys = x :(xs ++ ys)
